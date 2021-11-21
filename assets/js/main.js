57c509e7d2bf710f4eb55f7c5b64db89cc64494c
Jaseem-v <muhammedjaseem753@gmail.com>
2021-11-21 04:27:51 +0000

    mobile menu fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-/////////////////////////////////////////////////////
-// Mobile menu
-const openBtn = document.querySelector(".open-btn")
-const nav_list = document.querySelector(".navigation__list")
-const overlay = document.querySelector(".overlay")
-const navContact = document.querySelector(".nav-contact")
-
-function mobileMenu() {
-    openBtn.classList.toggle("active");
-    nav_list.classList.toggle("active")
-    overlay.classList.toggle("hidden")
-    navContact.classList.toggle("hidden")
-}
-
-openBtn.addEventListener("click", mobileMenu)
-overlay.addEventListener("click", mobileMenu)
-
 ///////////////////////////////////////
 // Navbar scroll
 const logo = document.querySelector(".navigation__logo img");
@@ -46,7 +29,22 @@ $(window).on('scroll', function () {
 });
 
 
+/////////////////////////////////////////////////////
+// Mobile menu
+const openBtn = document.querySelector(".open-btn")
+const nav_list = document.querySelector(".navigation__list")
+const overlay = document.querySelector(".overlay")
+const navContact = document.querySelector(".nav-contact")
 
+function mobileMenu() {
+    openBtn.classList.toggle("active");
+    nav_list.classList.toggle("active")
+    overlay.classList.toggle("hidden")
+    navContact.classList.toggle("hidden")
+}
+
+openBtn.addEventListener("click", mobileMenu)
+overlay.addEventListener("click", mobileMenu)
 
 
 ////////////////////////////////////////////////////
